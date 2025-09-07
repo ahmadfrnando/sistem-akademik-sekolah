4a779b6d8ef6998d5acfbfab1fa38f2db392968a
Ahmad Fernando <ahmadfernando88@gmail.com>
2025-09-07 15:29:00 +0700

    finish

--- database/factories/TanggapanMateriFactory.php
+++ database/factories/TanggapanMateriFactory.php
@@ -21,7 +21,7 @@ class TanggapanMateriFactory extends Factory
         return [
             'materi_id' => $materi->id,
             'tanggapan' => 'Bahasa indonesia adalah bahasa yang paling penting. dan menjadi bahasa persatuan yang paling penting di dinegeri.',
-            'file' => $this->faker->imageUrl(640, 480),
+            'file' => 'example/test.pdf',
             'siswa_id' => 1,
             'guru_id' => $materi->guru_id,
             'kelas_id' => $materi->kelas_id
